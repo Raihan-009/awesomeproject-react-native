f25d9e9a98c32c128dc14a8496d8b195bb68a742
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-06-16 11:08:05 +0600

    successfully printed hello world

--- src/App.js
+++ src/App.js
@@ -1,2 +1,22 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+
+
+export default function App(){
+    return(
+        <View style = {styles.container}>
+            <Text style = {styles.text}>Hello World!</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        backgroundColor : '#fff',
+    },
+    text : {
+        color : '#000000',
+
+    }
+})
