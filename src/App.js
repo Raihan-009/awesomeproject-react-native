746a1ab7820f34f40682ce820cab6128d2f5ef42
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-06-16 15:38:29 +0600

    input text box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet, TextInput } from "react-native";
 
 
 export default function App(){
+
+    const [text, setText] = useState('Hello World!')
+
     return(
         <View style = {styles.container}>
-            <Text style = {styles.text}>Hello World!</Text>
+            <Text style = {styles.text}>Enter Your Text</Text>
+            <TextInput 
+            style = {styles.input}
+            placeholder = 'e.g. Hello World!'
+            placeholderTextColor="#D3D3D3" 
+            textAlign="center"
+            caretHidden = {true}
+            onChangeText = {(val) => setText(val)}
+            />
+            <Text style = {styles.text2}> Demo Output : {text}</Text>
         </View>
     )
 }
@@ -14,9 +26,24 @@ const styles = StyleSheet.create({
     container : {
         flex : 1,
         backgroundColor : '#fff',
+        color : '#000000',
+        alignItems : "center",
+        justifyContent : "center",
     },
     text : {
         color : '#000000',
 
+    },
+    text2 : {
+        color : '#078f29',
+        fontSize : 20,
+    },
+    input :{
+        borderWidth : 1,
+        borderColor : '#777',
+        color : '#000000',
+        padding : 5,
+        margin : 10,
+        width : 180,
     }
 })
