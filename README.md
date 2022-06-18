09f8941c3573c45888b9fe268e40b8272bdda6ac
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-06-18 11:03:57 +0600

    brandview done

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ npm install @react-navigation/native @react-navigation/native-stack
 npm add react-native-gesture-handler
 react-native link react-native-gesture-handler
 ```
+
+## Necessary dependencies for Icons
+```
+react-native install react-native-vector-icons
+react-native link react-native-vector-icons
+npm install react-native-vector-icons --save
+react-native link react-native-vector-icons 
+react-native run-android 
+```
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,12 +1,50 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Dimensions, ImageBackground, ScrollView, Text, View, StyleSheet } from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const LoginScreen = () =>{
     return (
-        <View style = {{ flex:1, justifyContent:'center', alignItems : 'center' }}>
-            <Text style = {{ fontSize : 20, color : '#0a0a0a' }}>Log in Screen</Text>
-        </View>
+        <ScrollView style = {{ flex:1,backgroundColor:'#ffffff' }} showsVerticalScrollIndicator = {false}>
+            
+            <ImageBackground source={require('../../assets/images/abstract.jpeg')} style = {{ height : Dimensions.get('window').height / 2.5 }}>
+                <View style={styles.brandView}>
+                    <Icon name="location" style={{ fontSize:60, color:'#ffffff' }}></Icon>
+                    <Text style={styles.brandViewText}>Poridhi</Text>
+                </View>
+            </ImageBackground>
+           
+           <View style={styles.bottomView}>
+             <View style={{ padding:40 }}>
+                <Text style={{color:'#0850c4', fontSize:35, fontWeight:'500'}}>Welcome</Text>
+                <Text style={{color:'black'}}>Don't have any account?<Text style={{color:'red', fontStyle:'italic'}}> Register Now</Text></Text>
+                <View style={{marginTop:50}}>
+                    
+                </View>
+             </View>
+           </View>
+        </ScrollView>
     )
 }
 
 export default LoginScreen;
+
+
+const styles = StyleSheet.create({
+    brandView : {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    brandViewText :{
+         color : '#080808',
+         fontSize : 45,
+         fontWeight:"bold"
+    },
+    bottomView :{
+        flex:1.5,
+        backgroundColor:'white',
+        bottom:50,
+        borderTopStartRadius:60,
+        borderTopEndRadius:60,
+    }
+})
